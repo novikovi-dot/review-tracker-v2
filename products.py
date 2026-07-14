f6e546d9e5fa559a92e6998e0ea33b1b9a4bc5f8
novikovi-dot <novikovaksenia466@gmail.com>
2026-07-14 11:57:53 -0700

    Add new moisturizer product links to products.py

--- products.py
+++ products.py
@@ -3,5 +3,11 @@ PRODUCTS = {
         "Ulta": "https://www.ulta.com/p/banana-bright-vitamin-c-eye-creme-dark-circles-pimprod2042286?CATARGETID=330000200003198948&CAPCID=815135815341&CATCI=aud-818838764052:dsa-2297372631530&CAAGID=164159357434&CADevice=c&gad_source=1&gad_campaignid=21184057170&gbraid=0AAAAAD9rLH7rjGj6UqnSLE9fOB5lTjwHw&gclid=EAIaIQobChMIlJKS3qHElQMVbMvCBB07dwqpEAAYASAAEgL4iPD_BwE",
         "Sephora": "https://www.sephora.com/product/banana-bright-vitamin-c-eye-cre-me-P500613",
         "Brand Website": "https://olehenriksen.com/products/banana-bright-eye-creme"
+    },
+
+    "Strength Trainer Peptide Boost Moisturizer": {
+        "Ulta": "https://www.ulta.com/p/strength-peptide-barrier-boost-moisturizer-with-hyaluronic-acid-niacinamide-pimprod2042287",
+        "Sephora": "https://www.sephora.com/product/ole-henriksen-strength-trainer-peptide-boost-moisturizer-P479339",
+        "Brand Website": "https://olehenriksen.com/products/strength-trainer-peptide-boost-moisturizer"
     }
 }
