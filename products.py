3f9b33e47524e5f73f2aea2509b19d8dee924f1f
novikovi-dot <novikovaksenia466@gmail.com>
2026-07-22 09:02:13 -0700

    Simplify Ulta link for Banana Bright Eye Crème

--- products.py
+++ products.py
@@ -1,6 +1,6 @@
 PRODUCTS = {
     "Banana Bright+ Eye Crème": {
-        "Ulta": "https://www.ulta.com/p/banana-bright-vitamin-c-eye-creme-dark-circles-pimprod2042286?CATARGETID=330000200003198948&CAPCID=815135815341&CATCI=aud-818838764052:dsa-2297372631530&CAAGID=164159357434&CADevice=c&gad_source=1&gad_campaignid=21184057170&gbraid=0AAAAAD9rLH7rjGj6UqnSLE9fOB5lTjwHw&gclid=EAIaIQobChMIlJKS3qHElQMVbMvCBB07dwqpEAAYASAAEgL4iPD_BwE",
+        "Ulta": "https://www.ulta.com/p/banana-bright-vitamin-c-eye-creme-dark-circles-pimprod2042286",
         "Sephora": "https://www.sephora.com/product/banana-bright-vitamin-c-eye-cre-me-P500613",
         "Brand Website": "https://olehenriksen.com/products/banana-bright-eye-creme"
     },
